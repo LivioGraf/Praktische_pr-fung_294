1c89913804d09bff2207970b3c494266e1defd50
livio <livio05graf@gmail.com>
2022-10-04 11:30:29 +0200

    test

--- Praktische_prüfung_294/JS/script.js
+++ Praktische_prüfung_294/JS/script.js
@@ -1,5 +1,5 @@
 const table = document.createElement("table")
-const newTr = document.createElement("tr")
+const newTrForTh = document.createElement("tr")
 
 const ThId = document.createElement("th")
 ThId.innerText = "ID"
@@ -7,37 +7,42 @@ ThId.innerText = "ID"
 const ThTitle = document.createElement("th")
 ThTitle.innerText = "Title"
 
+const ThCompleted = document.createElement("th")
+ThCompleted.innerText = "Completed"
 
 
-newTr.appendChild(ThId)
-newTr.appendChild(ThTitle)
-table.appendChild(newTr)
+newTrForTh.appendChild(ThId)
+newTrForTh.appendChild(ThTitle)
+newTrForTh.appendChild(ThCompleted)
+table.appendChild(newTrForTh)
 document.body.appendChild(table)
 
-const data = fetch("http://localhost:3000/tasks")
-.then(function (data) {
-    return data.json()
-})
-.then(function (json) {
-    console.log(json)
-    for (i of json) {
-        let id = document.createElement("p")
-        id.innerText = i.id
+function readTask() {
+    const data = fetch("http://localhost:3000/tasks")
+    .then(function (data) {
+        return data.json()
+    })
+    .then(function (json) {
+        console.log(json)
+        for (i of json) {
+            let id = document.createElement("p")
+            id.innerText = i.id
 
-        let title = document.createElement("p")
-        title.innerText = i.title
+            let title = document.createElement("p")
+            title.innerText = i.title
 
-        let completed = document.createElement("p")
-        completed.innerText = i.completed
+            let completed = document.createElement("p")
+            completed.innerText = i.completed
 
-        document.body.appendChild(id)
-        document.body.appendChild(title)
-        document.body.appendChild(completed)
-    }
-})
+            document.body.appendChild(id)
+            document.body.appendChild(title)
+            document.body.appendChild(completed)
+        }
+    })
+}
 
-//let testTask = {id: 2, title: "Testtask", completed: false}
-//postRequest(testTask)
+//let testTask = {title: "Testtask", completed: false}
+//createTask(testTask)
 
 
 function createTask(newTask) {
@@ -56,6 +61,19 @@ function deleteTask(taskID) {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(taskID)
+        //body: JSON.stringify(taskID)
+    })
+}
+
+//let updatedTask = {id: 1, title: "Livio", completed: true}
+//updateTask(updatedTask)
+
+function updateTask(updatedTask) {
+    fetch("http://localhost:3000/tasks", {
+        method:"PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedTask)
     })
 }
